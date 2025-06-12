7e29917819026d6ad9858798da18c78e123c6903
Miguel Miguez <138310942+MiguelMiguez@users.noreply.github.com>
2025-06-11 22:46:07 -0300

    Update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import './Footer.css'
 import LogoVerde from '../../assets/logo/Brand Verde.svg'
+import { Link } from 'react-router-dom'
+import FacebookIcon from '../../assets/iconos/facebook.svg';
+import InstagramIcon from '../../assets/iconos/instagram.svg';
+import LinkedinIcon from '../../assets/iconos/linkedin.svg';
 
 const currentYear = () => new Date().getFullYear();
 
@@ -13,19 +17,38 @@ const Footer = () => {
                 </div>
                 <div className="ContainerNavbarFooter">
                     <ul>
-                        <li>CONTACTO</li>
-                        <li>NOSOTROS</li>
-                        <li>LOGISTICA</li>
+                        <li><Link to="/">INICIO</Link></li>
+                        <li><Link to="/insumos">INSUMOS</Link></li>
+                        <li><Link to="/acopio">ACOPIO</Link></li>
+                        <li><Link to="/logistica">LOGÍSTICA</Link></li>
+                        <li><Link to="/produccion">PRODUCCIÓN</Link></li>
+                        <li><Link to="/nosotros">NOSOTROS</Link></li>
+                        <li><Link to="/contacto">CONTACTO</Link></li>
                     </ul>
                 </div>
                 <div className="ContainerSocialMedia">
-                    <img src="/Iconos/facebook.svg" alt="Facebook" />
-                    <img src="/Iconos/instagram.svg" alt="Instagram" />
-                    <img src="/Iconos/linkedin.svg" alt="LinkedIn" />
+                    <a href="https://www.facebook.com/campofertilok" target="_blank" rel="noopener noreferrer">
+                        <img className='SocialIcon' src={FacebookIcon} alt="Facebook" />
+                    </a>
+                    <a href="https://www.instagram.com/campofertilvillaguay" target="_blank" rel="noopener noreferrer">
+                        <img className='SocialIcon' src={InstagramIcon} alt="Instagram" />
+                    </a>
+                    <a href="https://www.linkedin.com/company/campofertil" target="_blank" rel="noopener noreferrer">
+                        <img className='SocialIcon' src={LinkedinIcon} alt="LinkedIn" />
+                    </a>
                 </div>
             </div>
             <div className="ContainerCopy">
                 <p>© {currentYear()} CampoFertil. Todos los derechos reservados.</p>
+            </div>
+
+            <div className="ContainerCopy developer">
+                <p>
+                    Developed & designed by{" "}
+                    <a href="https://miguelmiguezporfolio.netlify.app/" target="_blank" rel="noopener noreferrer">
+                        mm
+                    </a>
+                </p>
             </div>
         </footer>
     )
